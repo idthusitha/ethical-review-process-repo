6bff4617b63dbd58ad82f659d9f19c8aa5ba9da8
thusitha <tdissanayaka@accelaero.com>
2021-11-07 21:00:26 +0100

    initial commit

---
+++ services/ethical-review-process-service/src/main/java/com/ethicalreview/ethicalreviewprocessservice/EthicalReviewProcessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ethicalreview.ethicalreviewprocessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthicalReviewProcessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthicalReviewProcessServiceApplication.class, args);
+	}
+
+}
